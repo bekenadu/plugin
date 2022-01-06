72ec3061165efa152ce2442344d28cbedeabab5a
bekenadu <it.beken.abate@gmail.com>
2022-01-06 16:25:49 +0300

    builing the basics

---
+++ wp-content/plugins/gallery/gallery.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+Plugin Name : gallery
+Plugin URI:
+Description : This Is a plugin made for small project as a demo
+Author: Beken
+Authour URI: https://youtube.com
+Version: 1:0
+License: GPLv2 or later
+Text Domain: gallery
+*/
+
+?>
