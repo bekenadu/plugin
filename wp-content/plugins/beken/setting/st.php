d1a6b7fa7b706f0375b71be3f6e82c0f97a5f064
bekenadu <it.beken.abate@gmail.com>
2022-01-18 16:42:09 +0300

    UI update and getting media from user

--- wp-content/plugins/beken/setting/st.php
+++ wp-content/plugins/beken/setting/st.php
@@ -1,4 +1,5 @@
 <div class="wrap">
     <h1>This is Gallery Plugin</h1>
     <p style="text-align:center; font-size:2rem;"> <span style="color:pink;">shortcode : </span> [gallary_shortcode]</p></br>
+    
 </div>
