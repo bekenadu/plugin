8c24d695425475c08ecab570a8ed63496c11ba3f
bekenadu <it.beken.abate@gmail.com>
2022-01-19 10:45:52 +0300

    storing image and accessing it

--- wp-content/plugins/beken/beken.php
+++ wp-content/plugins/beken/beken.php
@@ -85,6 +85,88 @@ wp_enqueue_media();
 </form><?php
 
 }
+add_action( 'admin_footer', 'store_file' );
+
+function store_file() {
+
+	$my_saved_attachment_post_id = get_option( 'media_selector_attachment_id', 0 );
+
+	?><script type='text/javascript'>
+
+		jQuery( document ).ready( function( $ ) {
+
+			// Uploading files
+			var file_frame;
+			var wp_media_post_id = wp.media.model.settings.post.id; // Store the old id
+			var set_to_post_id = <?php echo $my_saved_attachment_post_id; ?>; // Set this
+
+			jQuery('#upload_image_button').on('click', function( event ){
+
+				event.preventDefault();
+
+				// If the media frame already exists, reopen it.
+				if ( file_frame ) {
+					// Set the post ID to what we want
+					file_frame.uploader.uploader.param( 'post_id', set_to_post_id );
+					// Open frame
+					file_frame.open();
+					return;
+				} else {
+					// Set the wp.media post id so the uploader grabs the ID we want when initialised
+					wp.media.model.settings.post.id = set_to_post_id;
+				}
+
+				// Create the media frame.
+				file_frame = wp.media.frames.file_frame = wp.media({
+					title: 'Select a image to upload',
+					button: {
+						text: 'Use this image',
+					},
+					multiple: 'add',// Set to true to allow multiple files to be selected
+				});
+
+				// When an image is selected, run a callback.
+				file_frame.on( 'select', function() {
+					// We set multiple to false so only get one image from the uploader
+                    var selection = file_frame.state().get('selection');
+
+                var attachment_ids = selection.map( function( attachment ) {
+                    attachment = attachment.toJSON();
+                    return attachment;
+                });
+                console.log(attachment_ids);
+
+                    for(var i=0; i<attachment_ids.length;i++){
+                        var img = document.createElement('img');
+                        img.classList.add("img");
+            img.src = attachment_ids[i].url;
+           down =  document.getElementById('body').appendChild(img);
+            
+                        
+                    }
+
+                   
+
+
+					// $( '#image-preview' ).attr( 'src', attachment.url ).css( 'width', 'auto' );
+					// $( '#image_attachment_id' ).val( attachment.id );
+                   
+					// Restore the main post ID
+					wp.media.model.settings.post.id = wp_media_post_id;
+				});
+
+					// Finally, open the modal
+					file_frame.open();
+			});
+
+			// Restore the main ID when the add media button is pressed
+			jQuery( 'a.add_media' ).on( 'click', function() {
+				wp.media.model.settings.post.id = wp_media_post_id;
+			});
+		});
+
+	</script><?php
+}
 
 
 ?>
