8256dad322ee2a3eb7b60277837e0f2e652eeeb7
bekenadu <it.beken.abate@gmail.com>
2022-01-28 15:14:10 +0300

    slider pluging done

--- wp-content/plugins/beken/beken.php
+++ wp-content/plugins/beken/beken.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 /*
 Plugin Name: beken
 Plugin URI : www.a.com
@@ -10,174 +10,441 @@ license: GPL2
 if (!defined('ABSPATH')){
     die("Hey, what you think you doing");
 }
-
-
-
-function pluginTest_add_form(){
-    require_once plugin_dir_path(__FILE__).'setting/gallery.php';
-
+/**
+ * Create Settings Menu
+ */
+function beken()
+{
+    include_once plugin_dir_path(__FILE__) .'./setting/gallery.php';
 }
 
-add_shortcode('gallary_shortcode', 'pluginTest_add_form');
+// add shortcut 
+add_shortcode('gallery_1', 'beken');
 
+function beken_settings_menu() {
 
-function displayCode(){
-    require_once plugin_dir_path(__FILE__).'setting/st.php';
-
-}
-add_action( 'admin_menu', 'register_media_selector_settings_pages' );
-
-function register_media_selector_settings_pages() {
-    add_menu_page( 
-        "contact form", 
-        "Slider Form", 
-        "manage_options", 
-        "/test",
-        "media_selector_settings_page_callbacks",
-        "dashicons-cover-image",
-        '69'
+    $hook = add_menu_page(
+        __( 'beken Settings', 'my-plugin' ),
+        __( 'beken Settings', 'my-plugin' ),
+        'manage_options',
+        'beken-settings-page',
+        'beken_settings_template_callback',
+        '',
+        null
     );
-	// add_submenu_page( 'options-general.php', 'Media Selector', 'Media Selector', 'manage_options', 'media-selector', 'media_selector_settings_page_callback' );
+
+    add_action( 'admin_head-'.$hook, 'beken_image_uplaoder_assets', 10, 1 );
 }
+add_action('admin_menu', 'beken_settings_menu');
 
-function media_selector_settings_page_callbacks() {
-
-	// Save attachment ID
-	if ( isset( $_POST['submit_image_selector'] ) && isset( $_POST['image_attachment_id'] ) ) :
-		update_option( 'media_selector_attachment_id', absint( $_POST['image_attachment_id'] ) );
-	endif;
-
-	wp_enqueue_media();
-// image html
-	?><form method='post'>
-        <style>
-            .img{
-                height : 200px;
-                width: 200px;
-				padding :5px
-            }
-            </style>
-			
-		<div class='image-preview-wrapper' id="body">
-		<h1>Beken Slider</h1>
-			<h3>Select Attribute For Your Slider Carousel</h3>
-			<p> Beken Slider is the most powerful and intuitive WordPress plugin to create sliders which was never possible before.</p></br><br>
-
-			<label for="SliderTheme"><strong>Slider Theme</strong></label>
-  			<input type="Text" id="SliderTheme" name="SliderTheme" value ="Defualt" disabled ><br><br>
-			<label for="SliderDimention"><strong>Slider Dimention:</strong></label>
-			<input type="number" id="SliderDimention" name="SliderDimention">*
-  			<input type="number" id="SliderDimention1" name="SliderDimention1">px<br><br>
-			  <label for="speed"><strong>Slider Speed:</strong></label>
-  			<input type="number" id="shadow" name="shadow"><br><br>
-			  <label for="shadow"><strong>Shadow:</strong></label>
-            <select>
-                <option value="on">on</option>
-                <option value="off">off</option>
-        </select><br><br>
-
-        </div>
-		<input id="upload_image_button" type="button" class="button" value="<?php _e( 'Upload image' ); ?>" />
-		<input type='hidden' name='image_attachment_id' id='image_attachment_id' value='<?php echo get_option( 'media_selector_attachment_id' ); ?>'>
-		<input type="submit" name="submit_image_selector" value="Save" class="button-primary">
-	</form><?php
-     require_once plugin_dir_path(__FILE__).'setting/st.php';
-
+/**
+ * Enqueue Image Uploader Assets
+ */
+function beken_image_uplaoder_assets() {
+    wp_enqueue_media();
+    wp_enqueue_style( 'beken-image-uplaoder');
+    wp_enqueue_script( 'beken-image-uploader' );
 }
 
 
-add_action( 'admin_footer', 'media_selector_print_script' );
+/**
+ * Settings Template Page
+ */
+function beken_settings_template_callback() {
+    ?>
+    <div class="wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 
-function media_selector_print_script() {
+        <form action="options.php" method="post">
+            <?php 
+                // security field
+                settings_fields( 'beken-settings-page' );
 
-	$my_saved_attachment_post_id = get_option( 'media_selector_attachment_id', 0 );
+                // output settings section here
+                do_settings_sections('beken-settings-page');
 
-	?><script type='text/javascript'>
+                // save settings button
+                submit_button( 'Save Settings' );
+            ?>
+        </form>
+    </div>
+    <?php 
+}
 
-		jQuery( document ).ready( function( $ ) {
+/**
+ * Settings Template
+ */
+function beken_settings_init() {
 
-			// Uploading files
-			var file_frame;
-			var wp_media_post_id = wp.media.model.settings.post.id; // Store the old id
-			var set_to_post_id = <?php echo $my_saved_attachment_post_id; ?>; // Set this
+    // Setup settings section
+    add_settings_section(
+        'beken_settings_section',
+        'beken Settings Page',
+        '',
+        'beken-settings-page'
+    );
 
-			jQuery('#upload_image_button').on('click', function( event ){
+    // Register image uploader field
+    register_setting(
+        'beken-settings-page',
+        'beken_settings_image_uploader_field1',
+        array(
+            'type' => 'integer',
+            'sanitize_callback' => 'sanitize_image_uploader',
+            'default' => ''
+        )
+    );
 
-				event.preventDefault();
+	    // Add radio fields
+		add_settings_field(
+			'beken_settings_image_uploader_field1',
+			__( 'Image Uplaoder', 'my-plugin' ),
+			'beken_settings_image_uploader_field_callback1',
+			'beken-settings-page',
+			'beken_settings_section'
+		);
 
-				// If the media frame already exists, reopen it.
-				if ( file_frame ) {
-					// Set the post ID to what we want
-					file_frame.uploader.uploader.param( 'post_id', set_to_post_id );
-					// Open frame
-					file_frame.open();
-					return;
-				} else {
-					// Set the wp.media post id so the uploader grabs the ID we want when initialised
-					wp.media.model.settings.post.id = set_to_post_id;
-				}
+    // Register image uploader field
+    register_setting(
+        'beken-settings-page',
+        'beken_settings_image_uploader_field2',
+        array(
+            'type' => 'integer',
+            'sanitize_callback' => 'sanitize_image_uploader',
+            'default' => ''
+        )
+    );
 
-				// Create the media frame.
-				file_frame = wp.media.frames.file_frame = wp.media({
-					title: 'Select a image to upload',
-					button: {
-						text: 'Use this image',
-					},
-					multiple: 'add',// Set to true to allow multiple files to be selected
-				});
+	    // Add radio fields
+		add_settings_field(
+			'beken_settings_image_uploader_field2',
+			__( 'Image Uplaoder', 'my-plugin' ),
+			'beken_settings_image_uploader_field_callback2',
+			'beken-settings-page',
+			'beken_settings_section'
+		);
 
-				// When an image is selected, run a callback.
-				file_frame.on( 'select', function() {
-					// We set multiple to false so only get one image from the uploader
-                    var selection = file_frame.state().get('selection');
+		    // Register image uploader field
+			register_setting(
+				'beken-settings-page',
+				'beken_settings_image_uploader_field3',
+				array(
+					'type' => 'integer',
+					'sanitize_callback' => 'sanitize_image_uploader',
+					'default' => ''
+				)
+			);
+		
+				// Add radio fields
+				add_settings_field(
+					'beken_settings_image_uploader_field3',
+					__( 'Image Uplaoder', 'my-plugin' ),
+					'beken_settings_image_uploader_field_callback3',
+					'beken-settings-page',
+					'beken_settings_section'
+				);
 
-                $attachment_ids = selection.map( function( attachment ) {
-                    attachment = attachment.toJSON();
-                    return attachment;
-                });
-                console.log( $attachment_ids );
-                 $len =  $attachment_ids .length;
-                 <?php
-                 $lens = $len; 
-                    $urls = array(  );
-                    for($i=0; $i<$len; $i++){
-
-                        $urls[i] =   $attachment_ids [i].url;
-                    }
-                    ?>
-
-                    for(var i=0; i<len;i++){
-                        var img = document.createElement('img');
-                        img.classList.add("img");
-            img.src =  $attachment_ids [i].url;
-           down =  document.getElementById('body').appendChild(img);
-
-            
-                        
-                    }
-                  
-
-                   
-
-
-					// $( '#image-preview' ).attr( 'src', attachment.url ).css( 'width', 'auto' );
-					// $( '#image_attachment_id' ).val( attachment.id );
-                   
-					// Restore the main post ID
-					wp.media.model.settings.post.id = wp_media_post_id;
-				});
-
-					// Finally, open the modal
-					file_frame.open();
-			});
-
-			// Restore the main ID when the add media button is pressed
-			jQuery( 'a.add_media' ).on( 'click', function() {
-				wp.media.model.settings.post.id = wp_media_post_id;
-			});
-		});
-
-	</script><?php
+				    // Register image uploader field
+					register_setting(
+						'beken-settings-page',
+						'beken_settings_image_uploader_field4',
+						array(
+							'type' => 'integer',
+							'sanitize_callback' => 'sanitize_image_uploader',
+							'default' => ''
+						)
+					);
+				
+						// Add radio fields
+						add_settings_field(
+							'beken_settings_image_uploader_field4',
+							__( 'Image Uplaoder', 'my-plugin' ),
+							'beken_settings_image_uploader_field_callback4',
+							'beken-settings-page',
+							'beken_settings_section'
+						);
 
 }
+add_action( 'admin_init', 'beken_settings_init' );
+
+/**
+ * Sanitize Image Uploader
+ */
+function sanitize_image_uploader( $value ) {
+    if(isset($value)) {
+        return intval($value);
+    }else {
+        return false;
+    }
+}
+
 ?>
+    <?php 
+
+
+/**
+ * Image Uploader Template
+ */
+function beken_settings_image_uploader_field_callback1() {
+
+    $beken_image_id = get_option('beken_settings_image_uploader_field1');
+
+    ?>
+    <div class="beken-upload-wrap">
+        <img data-src="" src="<?php echo esc_url(wp_get_attachment_url(isset($beken_image_id) ? (int) $beken_image_id : 0)); ?>" width="250" />
+        <div class="beken-upload-action">
+            <input type="hidden" name="beken_settings_image_uploader_field1" value="<?php echo esc_attr(isset($beken_image_id) ? (int) $beken_image_id : 0); ?>" />
+            <button type="button" class="upload_image_button"><span class="dashicons dashicons-plus"></span></button>
+            <button type="button" class="remove_image_button"><span class="dashicons dashicons-minus"></span></button>
+        </div>
+    </div>
+	<script>
+	 ;(function($) {
+    'use strict';
+    $(document.body).on('click', '.upload_image_button', function() {
+
+        var send_attachment_bkp = wp.media.editor.send.attachment;
+
+        var button = $(this);
+
+        wp.media.editor.send.attachment = function(props, attachment) {
+
+            $(button).parent().prev().attr('src', attachment.url);
+
+            $(button).prev().val(attachment.id);
+
+            wp.media.editor.send.attachment = send_attachment_bkp;
+
+        }
+
+        wp.media.editor.open(button);
+
+        return false;
+
+    });
+
+
+
+    // The "Remove" button (remove the value from input type='hidden')
+
+    $(document.body).on('click', '.remove_image_button', function() {
+
+        var answer = confirm('Are you sure?');
+
+        if (answer == true) {
+
+            var src = $(this).parent().prev().attr('data-src');
+
+            $(this).parent().prev().attr('src', src);
+
+            $(this).prev().prev().val('');
+
+        }
+
+        return false;
+
+    });
+
+})(jQuery);
+ </script>
+    <?php 
+}
+
+
+function beken_settings_image_uploader_field_callback2() {
+
+    $beken_image_id = get_option('beken_settings_image_uploader_field2');
+
+    ?>
+    <div class="beken-upload-wrap">
+        <img data-src="" src="<?php echo esc_url(wp_get_attachment_url(isset($beken_image_id) ? (int) $beken_image_id : 0)); ?>" width="250" />
+        <div class="beken-upload-action">
+            <input type="hidden" name="beken_settings_image_uploader_field2" value="<?php echo esc_attr(isset($beken_image_id) ? (int) $beken_image_id : 0); ?>" />
+            <button type="button" class="upload_image_button1"><span class="dashicons dashicons-plus"></span></button>
+            <button type="button" class="remove_image_button1"><span class="dashicons dashicons-minus"></span></button>
+        </div>
+    </div>
+	<script>
+	 ;(function($) {
+    'use strict';
+    $(document.body).on('click', '.upload_image_button1', function() {
+
+        var send_attachment_bkp = wp.media.editor.send.attachment;
+
+        var button = $(this);
+
+        wp.media.editor.send.attachment = function(props, attachment) {
+
+            $(button).parent().prev().attr('src', attachment.url);
+
+            $(button).prev().val(attachment.id);
+
+            wp.media.editor.send.attachment = send_attachment_bkp;
+
+        }
+
+        wp.media.editor.open(button);
+
+        return false;
+
+    });
+
+
+
+    // The "Remove" button (remove the value from input type='hidden')
+
+    $(document.body).on('click', '.remove_image_button1', function() {
+
+        var answer = confirm('Are you sure?');
+
+        if (answer == true) {
+
+            var src = $(this).parent().prev().attr('data-src');
+
+            $(this).parent().prev().attr('src', src);
+
+            $(this).prev().prev().val('');
+
+        }
+
+        return false;
+
+    });
+
+})(jQuery);
+ </script>
+    <?php 
+}
+
+
+function beken_settings_image_uploader_field_callback3() {
+
+    $beken_image_id = get_option('beken_settings_image_uploader_field3');
+
+    ?>
+    <div class="beken-upload-wrap">
+        <img data-src="" src="<?php echo esc_url(wp_get_attachment_url(isset($beken_image_id) ? (int) $beken_image_id : 0)); ?>" width="250" />
+        <div class="beken-upload-action">
+            <input type="hidden" name="beken_settings_image_uploader_field3" value="<?php echo esc_attr(isset($beken_image_id) ? (int) $beken_image_id : 0); ?>" />
+            <button type="button" class="upload_image_button2"><span class="dashicons dashicons-plus"></span></button>
+            <button type="button" class="remove_image_button2"><span class="dashicons dashicons-minus"></span></button>
+        </div>
+    </div>
+	<script>
+	 ;(function($) {
+    'use strict';
+    $(document.body).on('click', '.upload_image_button2', function() {
+
+        var send_attachment_bkp = wp.media.editor.send.attachment;
+
+        var button = $(this);
+
+        wp.media.editor.send.attachment = function(props, attachment) {
+
+            $(button).parent().prev().attr('src', attachment.url);
+
+            $(button).prev().val(attachment.id);
+
+            wp.media.editor.send.attachment = send_attachment_bkp;
+
+        }
+
+        wp.media.editor.open(button);
+
+        return false;
+
+    });
+
+
+
+    // The "Remove" button (remove the value from input type='hidden')
+
+    $(document.body).on('click', '.remove_image_button2', function() {
+
+        var answer = confirm('Are you sure?');
+
+        if (answer == true) {
+
+            var src = $(this).parent().prev().attr('data-src');
+
+            $(this).parent().prev().attr('src', src);
+
+            $(this).prev().prev().val('');
+
+        }
+
+        return false;
+
+    });
+
+})(jQuery);
+ </script>
+    <?php 
+}
+
+
+function beken_settings_image_uploader_field_callback4() {
+
+    $beken_image_id = get_option('beken_settings_image_uploader_field4');
+
+    ?>
+    <div class="beken-upload-wrap">
+        <img data-src="" src="<?php echo esc_url(wp_get_attachment_url(isset($beken_image_id) ? (int) $beken_image_id : 0)); ?>" width="250" />
+        <div class="beken-upload-action">
+            <input type="hidden" name="beken_settings_image_uploader_field4" value="<?php echo esc_attr(isset($beken_image_id) ? (int) $beken_image_id : 0); ?>" />
+            <button type="button" class="upload_image_button3"><span class="dashicons dashicons-plus"></span></button>
+            <button type="button" class="remove_image_button3"><span class="dashicons dashicons-minus"></span></button>
+        </div>
+    </div>
+	<script>
+	 ;(function($) {
+    'use strict';
+    $(document.body).on('click', '.upload_image_button3', function() {
+
+        var send_attachment_bkp = wp.media.editor.send.attachment;
+
+        var button = $(this);
+
+        wp.media.editor.send.attachment = function(props, attachment) {
+
+            $(button).parent().prev().attr('src', attachment.url);
+
+            $(button).prev().val(attachment.id);
+
+            wp.media.editor.send.attachment = send_attachment_bkp;
+
+        }
+
+        wp.media.editor.open(button);
+
+        return false;
+
+    });
+
+
+
+    // The "Remove" button (remove the value from input type='hidden')
+
+    $(document.body).on('click', '.remove_image_button3', function() {
+
+        var answer = confirm('Are you sure?');
+
+        if (answer == true) {
+
+            var src = $(this).parent().prev().attr('data-src');
+
+            $(this).parent().prev().attr('src', src);
+
+            $(this).prev().prev().val('');
+
+        }
+
+        return false;
+
+    });
+
+})(jQuery);
+ </script>
+    <?php 
+}
+ 
--- wp-content/plugins/beken/setting/gallery.php
+++ wp-content/plugins/beken/setting/gallery.php
@@ -1,30 +1,81 @@
 
-  <div id="carousel" class="carousel slide" data-ride="carousel">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
 
-    <ol class="carousel-indicators">
-
-    </ol>
-    <div class="carousel-inner" action="/action_page.php">
-    <div class="carousel-item active">
-
-     
-              
-                <?php 
-          
-          for($x =0 ;$x<$;$x++){
-              echo '  <div class="carousel-item active">
-              <img class="d-block" src="'.$urls[$x].'" >
-              </div>';
-          }
-          ?>
-
-    </div>
-    <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
+  <div >
+    <?php
+    $beken_image_id1 = get_option('beken_settings_image_uploader_field1');
+    $beken_image_id2 = get_option('beken_settings_image_uploader_field2');
+    $beken_image_id3 = get_option('beken_settings_image_uploader_field3');
+    $beken_image_id4 = get_option('beken_settings_image_uploader_field4');
+    
+     ?>
   </div>
+
+  <?php if(isset($beken_image_id1))
+  {
+    ?>
+
+<div class="carousel-item active">
+      <img class="d-block w-100" src="<?php echo esc_url(wp_get_attachment_url(isset($beken_image_id1) ? (int) $beken_image_id1 : 0)); ?>" alt="First slide">
+    </div>
+<?php
+  } 
+?>
+
+<?php if(isset($beken_image_id2))
+  {
+    ?>
+
+<div class="carousel-item ">
+      <img class="d-block w-100" src="<?php echo esc_url(wp_get_attachment_url(isset($beken_image_id2) ? (int) $beken_image_id2 : 0)); ?>" alt="First slide">
+    </div>
+
+<?php
+  } 
+?>
+
+<?php if(isset($beken_image_id3))
+  {
+    ?>
+
+<div class="carousel-item ">
+      <img class="d-block w-100" src="<?php echo esc_url(wp_get_attachment_url(isset($beken_image_id3) ? (int) $beken_image_id3 : 0)); ?>" alt="First slide">
+    </div>
+<?php
+  } 
+?>
+
+<?php if(isset($beken_image_id4))
+  {
+    ?>
+
+<div class="carousel-item ">
+      <img class="d-block w-100" src="<?php echo esc_url(wp_get_attachment_url(isset($beken_image_id4) ? (int) $beken_image_id4 : 0)); ?>" alt="First slide">
+    </div>
+<?php
+  } 
+?>
+
+
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+      
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+
+
+ 
